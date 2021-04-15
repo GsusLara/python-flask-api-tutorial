eb56a59754dc76cf4e6cb6af963dec35783e012e
Jesus Lara <jelara99@gmail.com>
2021-04-15 21:40:04 +0000

    endpoint creado

---
+++ src/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+app  = Flask(__name__)
+import json
+
+todos = [
+    { "label": "My first task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    decoded_object = json.loads(request.data)
+    todos.append(decoded_object)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    return 'something'
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
